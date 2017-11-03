fa573daf6f1b586aa7d4598fa76e740062f596a5
xiajunhua <18810796504@163.com>
2017-11-03 14:59:34 +0800

    ?-->+ add  index

---
+++ webDemo01/src/java/com/mysql/demo/Student.java
@@ -0,0 +1,27 @@
+package com.mysql.demo;
+
+public class Student {
+
+	private int id;
+	private String name;
+	private String job;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+}
